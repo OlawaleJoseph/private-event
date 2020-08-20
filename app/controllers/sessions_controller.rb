c5657d842c2027615b54900f588f0b72a512f745
OlawaleJoseph <adedekojoseph@gmail.com>
2020-08-20 15:25:43 +0100

    authenticate users before creating an event

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
   def new; end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user
   before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users
