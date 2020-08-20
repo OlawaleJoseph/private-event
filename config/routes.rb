72b168eedea20346a5ce481cca4752f5e4dac524
kiptubei <kiptubei@gmail.com>
2020-08-20 14:57:41 +0300

    Added capybara feature tests for view users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: %i[new create destroy]
   resources :events, only: %i[new create show index]
-  post '/events/:id/attend', to: 'events#attend' 
+  post '/events/:id/attend', to: 'events#attend'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,8 +6,8 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-
-
+require 'capybara/rails'
+# require 'env'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -45,6 +45,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
@@ -72,4 +73,3 @@ RSpec.configure do |config|
     end
   end
 end
-
